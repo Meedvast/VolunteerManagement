56d62adb359c08ec9eb955c154787aa171a6dbe5
Meedvast <Meedvast@gmail.com>
2022-06-02 22:08:21 +0800

    亚运会志愿者管理系统

---
+++ cmake-build-debug/CMakeFiles/VolunteerManagement.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VolunteerManagement.dir/main.c.obj"
+  "CMakeFiles/VolunteerManagement.dir/main.c.obj.d"
+  "VolunteerManagement.exe"
+  "VolunteerManagement.exe.manifest"
+  "VolunteerManagement.pdb"
+  "libVolunteerManagement.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/VolunteerManagement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/VolunteerManagement.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for VolunteerManagement.
+# This may be replaced when dependencies are built.
